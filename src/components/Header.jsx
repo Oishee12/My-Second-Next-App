51e60c19e185c9df91eddcc771371779cd70e0bf
Oishe <yhsiokhan@gmail.com>
2026-07-27 20:28:54 +0600

    Delete default page and add header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HeaderPage = () => {
+  return (
+    <div>
+        <h2 className='text-red-400 text-5xl'>My awesome header is here</h2>
+    </div>
+  )
+}
+
+export default HeaderPage
