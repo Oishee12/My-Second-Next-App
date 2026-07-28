af0903a938b4af4d2c14d84ebfe4353ebaed1bc2
Oishe <yhsiokhan@gmail.com>
2026-07-28 23:18:12 +0600

    Add food page and fetch data from api

---
+++ src/app/foods/page.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const FoodsPage = async() => {
+  const res = await fetch('https://phi-lab-server.vercel.app/api/v1/lab/foods');
+  const data = await res.json();
+  const foods = await data.data;
+  console.log(foods);
+    return (
+    <div>
+        <h2>Foods: {foods.length}</h2>
+    </div>
+  )
+}
+
+export default FoodsPage
