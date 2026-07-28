c77ac9fb4c00cd8304f0af06dcf4646631fea35e
Oishe <yhsiokhan@gmail.com>
2026-07-28 23:37:16 +0600

    Setup daisyUI

--- src/app/foods/page.jsx
+++ src/app/foods/page.jsx
@@ -4,7 +4,7 @@ const FoodsPage = async() => {
   const res = await fetch('https://phi-lab-server.vercel.app/api/v1/lab/foods');
   const data = await res.json();
   const foods = await data.data;
-  console.log(foods);
+  
     return (
     <div>
         <h2>Foods: {foods.length}</h2>
