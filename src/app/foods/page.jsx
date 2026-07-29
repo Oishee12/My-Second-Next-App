494b6b8dc8f5bebd429a787c78b1aa2a3d074070
Oishe <yhsiokhan@gmail.com>
2026-07-29 19:39:34 +0600

    Add card from daisyUI

--- src/app/foods/page.jsx
+++ src/app/foods/page.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import FoodCard from "@/components/FoodCard";
 
 const FoodsPage = async() => {
   const res = await fetch('https://phi-lab-server.vercel.app/api/v1/lab/foods');
@@ -8,6 +8,9 @@ const FoodsPage = async() => {
     return (
     <div>
         <h2>Foods: {foods.length}</h2>
+        {
+          foods.map(food => (<FoodCard food={food} key={food.id}></FoodCard>))
+        }
     </div>
   )
 }
