4973e9b8a07d1e968c148175deb01b851d3341ab
Oishe <yhsiokhan@gmail.com>
2026-07-30 16:59:46 +0600

    Show details button clickable and fetch food details api

--- src/app/foods/[foodId]/page.jsx
+++ src/app/foods/[foodId]/page.jsx
@@ -1,9 +1,21 @@
+import Image from "next/image";
+
 const FoodDetailsPage = async({params}) => {
     const {foodId} = await params;
+const res = await fetch(`https://phi-lab-server.vercel.app/api/v1/lab/foods/${foodId}`);
+const data = await res.json();
+const {id, dish_name, image_link} = data.data;
 
   return (
     <div>
         <h2>Show details of food: {foodId}</h2>
+        <h3 className="text-3xl">Dish Name: {dish_name}</h3>
+        <Image
+        src= {image_link}
+        alt= {dish_name}
+        width={300}
+        height={300}
+        ></Image>
     </div>
   )
 }
