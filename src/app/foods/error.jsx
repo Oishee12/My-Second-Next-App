7a9833d36d966e4644efa725103386259b918ea7
Oishe <yhsiokhan@gmail.com>
2026-07-30 17:26:36 +0600

    Add customized error at food page

---
+++ src/app/foods/error.jsx
@@ -0,0 +1,12 @@
+'use client'
+const error = () => {
+  return (
+    <div>
+        <h2 className='text-3xl text-red-500'>
+            Something went wrong! Please try again later.
+        </h2>
+    </div>
+  )
+}
+
+export default error
